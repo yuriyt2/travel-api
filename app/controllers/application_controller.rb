7d339b54556558914f15c342d90e64fa3790272f
Yuriy <yuriy.turetskiy@gmail.com>
2017-04-04 08:55:43 -0400

    remove preflight stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :cors_preflight_check
   after_filter :set_access_control_headers
 
    def set_access_control_headers
@@ -13,14 +12,5 @@ class ApplicationController < ActionController::Base
      headers['Access-Control-Max-Age'] = "1728000"
    end
 
-def cors_preflight_check
-  if request.method == :options
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = '*'
-    headers['Access-Control-Max-Age'] = '1728000'
-    render :text => '', :content_type => 'text/plain'
-  end
-end
 
 end
