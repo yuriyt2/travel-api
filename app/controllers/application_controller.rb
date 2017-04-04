84203d9cb586a1936679b630becaf01366f5b897
Yuriy <yuriy.turetskiy@gmail.com>
2017-04-04 08:49:42 -0400

    set headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
    def set_access_control_headers
      headers['Access-Control-Allow-Origin'] = '*'
      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+     headers['Access-Control-Allow-Headers'] = '*'
    end
 
 
