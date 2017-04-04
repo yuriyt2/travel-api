932b6c509a661100e76447687a87d57d0b93a2fd
Yuriy <yuriy.turetskiy@gmail.com>
2017-04-04 08:51:05 -0400

    age

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,9 @@ class ApplicationController < ActionController::Base
 
    def set_access_control_headers
      headers['Access-Control-Allow-Origin'] = '*'
-     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+     headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS'
      headers['Access-Control-Allow-Headers'] = '*'
+     headers['Access-Control-Max-Age'] = "1728000"
    end
 
 
