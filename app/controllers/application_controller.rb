e382705dab6576be4acf08e1d9a963d4a32dc96c
Yuriy <yuriy.turetskiy@gmail.com>
2020-03-21 15:36:55 -0400

    last few gem updates for rails 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate
-  after_filter :set_access_control_headers
+  after_action :set_access_control_headers
 
    def set_access_control_headers
      headers['Access-Control-Allow-Origin'] = '*'
